a62a9cf08f9c0ea8a8cb445bb6c292ba8fd8f9bd
Patrick <pateast678@gmail.com>
2019-11-12 13:47:36 -0500

    testing commiting, pushing, and pulling from windows to linux VM

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hayyyy y'all")
+# cats_name = "Oakley"
+# print(f"Hayyyy {cats_name.upper()}")
+
+cats = ['Oakley', 'Milla']
+
+def greet(cat):
+    print(f'Hello, {cat.upper()}')
+
+for cat in cats:
+    greet(cat)
+greet('pat')
+# greet() throws error, needs an argument
+greet('')
